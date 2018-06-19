5569b666a143ce9fc69672eecfc472fb1e5318ad
Laurie Carano <lauriecarano@gmail.com>
2018-06-18 22:27:59 -0600

    4th commit - accessed limbo

--- js/app.js
+++ js/app.js
@@ -80,42 +80,61 @@
 // 3rd commit - performed operations on the crazy object
 
 
-const crazyObject = {
-  taco: [{meat: 'steak',
-         cheese: ['panela', 'queso', 'chihuahua']},
+// const crazyObject = {
+//   taco: [{meat: 'steak',
+//          cheese: ['panela', 'queso', 'chihuahua']},
          
-         {meat: 'chicken',
-          salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
-        ],
-  larry: {
-    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
-    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
-    characters: [{
-      name: "Jeff",
-      occupation: "manager"
-    },
-    {
-      name: "funkhauser",
-      occupation: "tv dude"
-    },
-    {
-      name: "susie",
-      occupation: "jeffs wife",
-      favourtieHobby: "Swearing at Larry and Jeff"
-    },
-    ]
-  }
+//          {meat: 'chicken',
+//           salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+//         ],
+//   larry: {
+//     nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+//     quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+//     characters: [{
+//       name: "Jeff",
+//       occupation: "manager"
+//     },
+//     {
+//       name: "funkhauser",
+//       occupation: "tv dude"
+//     },
+//     {
+//       name: "susie",
+//       occupation: "jeffs wife",
+//       favourtieHobby: "Swearing at Larry and Jeff"
+//     },
+//     ]
+//   }
+// }
+
+// console.log(crazyObject.taco[1].salsa[5]);
+// console.log(crazyObject.larry.quotes[0]);
+// console.log(crazyObject.larry.characters[2].favourtieHobby);
+// console.log(crazyObject.larry.nicknames[1]);
+// console.log(crazyObject.larry.characters[1].name);
+
+
+// 4th commit - accessed limbo
+
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
 }
 
-console.log(crazyObject.taco[1].salsa[5]);
-console.log(crazyObject.larry.quotes[0]);
-console.log(crazyObject.larry.characters[2].favourtieHobby);
-console.log(crazyObject.larry.nicknames[1]);
-console.log(crazyObject.larry.characters[1].name);
-
-
-
-
+console.log(inception.limbo = "null");
 
 
 
