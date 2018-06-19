0ad9587a1aea99dcf22f1abcc99a25c864268999
Laurie Carano <lauriecarano@gmail.com>
2018-06-18 22:00:01 -0600

    2nd commit - created some data structures and accessed some data

---
+++ js/app.js
@@ -0,0 +1,136 @@
+// console.log('running');
+
+// 1st Commit - Selecting datatypes
+// write datatypes you would use to represent the data, & give small example of data structure
+
+// A light switch that can be either on or off.
+// booleans
+// let lightSwitchOn(true)
+
+
+
+
+// A user's email address.
+//  a string
+// 'name@email.com'
+
+
+
+
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// An object
+// const spaceship = {
+// 	hull: 'metal',
+// 	laserBlasters: 4,
+// 	tractorBeam: 4
+
+
+
+
+// A list of student names from our class.
+// An array
+
+// const students = ['someone', 'someone', 'someone'];
+
+
+
+
+// A list of student names from our class, each with a location.
+// an array
+
+// const students = ['someone, COS', ]
+
+
+
+
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+
+// Array of objects
+
+// const generalAssembly = {
+	
+// 	students: [{
+// 		name: "laurie",
+// 		location: "COS",
+// 		favortieTvShows: "Silicon Valley",
+// 	},{
+// 		name: "laurie",
+// 		location: "COS",
+// 		favortieTvShows: "Silicon Valley",
+// 	},{
+
+// 	}]
+	
+// }
+
+// 2nd commit - created some data structures and accessed some data
+
+const rainbow = ['red', 'orange', 'blue', 'yellow', 'purple', 'green'];
+console.log(rainbow[2]);
+
+
+const laurie = {
+	favoriteFood: 'pasta',
+	hobby: 'pottery',
+	town: 'cos',
+	dataType: 'array'
+}
+console.log(laurie.hobby);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
